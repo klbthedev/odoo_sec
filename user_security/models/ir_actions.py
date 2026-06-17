df995f830289b2a55603f497749e0db0a885982b
klbthedev <root@DESKTOP-GBQ5UHD.localdomain>
2026-06-17 15:16:02 +0300

    patch for user list

--- user_security/models/ir_actions.py
+++ user_security/models/ir_actions.py
@@ -26,20 +26,105 @@ class SecurityActionMixin(models.AbstractModel):
 
 
 class IrActionsActWindow(models.Model):
-    _inherit = ["ir.actions.act_window", "security.action.mixin"]
+    _inherit = "ir.actions.act_window"
+
+    def _check_security_admin(self):
+        if (self.env.su or self.env.user.has_group("user_security.group_security_admin") or self.env.user.has_group("base.group_system")):
+            return
+        raise AccessError("Action modification denied.")
+
+    def create(self, vals):
+        self._check_security_admin()
+        return super().create(vals)
+
+    def write(self, vals):
+        self._check_security_admin()
+        return super().write(vals)
+
+    def unlink(self):
+        self._check_security_admin()
+        return super().unlink()
 
 
-class IrActionsServer(models.Model):
-    _inherit = ["ir.actions.server", "security.action.mixin"]
+class IrActionsActWindow(models.Model):
+    _inherit = "ir.actions.server"
+
+    def _check_security_admin(self):
+        if (self.env.su or self.env.user.has_group("user_security.group_security_admin") or self.env.user.has_group("base.group_system")):
+            return
+        raise AccessError("Action modification denied.")
+
+    def create(self, vals):
+        self._check_security_admin()
+        return super().create(vals)
+
+    def write(self, vals):
+        self._check_security_admin()
+        return super().write(vals)
+
+    def unlink(self):
+        self._check_security_admin()
+        return super().unlink()
 
 
-class IrActionsClient(models.Model):
-    _inherit = ["ir.actions.client", "security.action.mixin"]
+class IrActionsActWindow(models.Model):
+    _inherit = "ir.actions.client"
+
+    def _check_security_admin(self):
+        if (self.env.su or self.env.user.has_group("user_security.group_security_admin") or self.env.user.has_group("base.group_system")):
+            return
+        raise AccessError("Action modification denied.")
+
+    def create(self, vals):
+        self._check_security_admin()
+        return super().create(vals)
+
+    def write(self, vals):
+        self._check_security_admin()
+        return super().write(vals)
+
+    def unlink(self):
+        self._check_security_admin()
+        return super().unlink()
+
+class IrActionsActWindow(models.Model):
+    _inherit = "ir.actions.report"
+
+    def _check_security_admin(self):
+        if (self.env.su or self.env.user.has_group("user_security.group_security_admin") or self.env.user.has_group("base.group_system")):
+            return
+        raise AccessError("Action modification denied.")
+
+    def create(self, vals):
+        self._check_security_admin()
+        return super().create(vals)
+
+    def write(self, vals):
+        self._check_security_admin()
+        return super().write(vals)
+
+    def unlink(self):
+        self._check_security_admin()
+        return super().unlink()
+
+class IrActionsActWindow(models.Model):
+    _inherit = "ir.actions.act_url"
+
+    def _check_security_admin(self):
+        if (self.env.su or self.env.user.has_group("user_security.group_security_admin") or self.env.user.has_group("base.group_system")):
+            return
+        raise AccessError("Action modification denied.")
+
+    def create(self, vals):
+        self._check_security_admin()
+        return super().create(vals)
+
+    def write(self, vals):
+        self._check_security_admin()
+        return super().write(vals)
+
+    def unlink(self):
+        self._check_security_admin()
+        return super().unlink()
 
 
-class IrActionsReport(models.Model):
-    _inherit = ["ir.actions.report", "security.action.mixin"]
-
-
-class IrActionsUrl(models.Model):
-    _inherit = ["ir.actions.act_url", "security.action.mixin"]
