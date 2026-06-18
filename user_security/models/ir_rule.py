22c0019838b4a1830a88fdc535c856f81bcf135f
klbthedev <root@DESKTOP-GBQ5UHD.localdomain>
2026-06-18 11:04:03 +0300

    user security module changes

--- user_security/models/ir_rule.py
+++ user_security/models/ir_rule.py
@@ -5,26 +5,6 @@ from odoo.exceptions import AccessError
 class IrRule(models.Model):
     _inherit = "ir.rule"
 
-    def _check_security_admin(self):
-        if self.env.su:
-            return
-        if self.env.user.has_group("user_security.group_security_admin"):
-            return
-        raise AccessError("Record rule modification denied.")
-
-    def create(self, vals):
-        self._check_security_admin()
-        return super().create(vals)
-
-    def write(self, vals):
-        self._check_security_admin()
-        return super().write(vals)
-
-    def unlink(self):
-        self._check_security_admin()
-        return super().unlink()
-
-    
     def _check(self):
         if (not self.env.su and not self.env.user.has_group('user_security.group_security_admin')):
             raise AccessError("Rule modification denied.")
--- user_security/models/res_users.py
+++ user_security/models/res_users.py
@@ -5,39 +5,6 @@ from odoo.exceptions import AccessError
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    # PROTECTED_FIELDS = {
-    #     "groups_id",
-    #     "active",
-    #     "company_id",
-    #     "company_ids",
-    #     "share",
-    # }
-
-    # PROTECTED_USERS = {
-    #     1,
-    #     2,
-    # }
-
-    # def _check_security_admin(self):
-    #     if self.env.su:
-    #         return
-    #     if self.env.user.has_group("user_security.group_security_admin"):
-    #         return
-    #     raise AccessError("Only Security Administrators may modify security settings.")
-
-    # def write(self, vals):
-    #     if self.PROTECTED_FIELDS.intersection(vals):
-    #         self._check_security_admin()
-    #     if any(user.id in self.PROTECTED_USERS for user in self):
-    #         self._check_security_admin()
-    #     return super().write(vals)
-
-    # def unlink(self):
-    #     if any(user.id in self.PROTECTED_USERS for user in self):
-    #         self._check_security_admin()
-    #     return super().unlink()
-
-
     security_protected = fields.Boolean(default=False,copy=False)
 
     SENSITIVE_FIELDS = {
@@ -84,9 +51,6 @@ class ResUsers(models.Model):
         if sensitive.intersection(vals) and not self._is_security_admin():
             raise AccessError("Security modification denied.")
         return super().write(vals)
-        # if (self.SENSITIVE_FIELDS.intersection(vals) and not self._is_security_admin()):
-        #     raise AccessError("Security modification denied.")
-        # return super().write(vals)
 
     def unlink(self):
         self._check_protected()
